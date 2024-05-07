b5d3506593058b22fca15d4cfc55a0e2903e2a86
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-07 11:29:28 +0200

    [FIX] proofreading

--- snack-4/js/main.js
+++ snack-4/js/main.js
@@ -4,21 +4,20 @@ fino a quando ne avrà tanti quanti l’altro.*/
 
 "use strict";
 
-const lista1 = [3, 5, 1, 8, 2, 4, 5]
-const lista2 = [0, 5, 2, 8, 5, 4, 7, 6, 1, 2, 4]
+const lista1 = [];
+const lista2 = [2, 3, 4, 5];
+
+let minArray;
 
 if (lista1.length < lista2.length){
-    while (lista1.length < lista2.length){
-        lista1.push(Math.ceil(Math.random() * 10))
-    }
-} else {
-    while (lista2.length < lista1.length){
-        lista2.push(Math.ceil(Math.random() * 10))
-    }
+    minArray = lista1;
+} else if (lista2.length < lista1.length) {
+    minArray = lista2;
 }
 
-console.log(lista1)
-console.log(lista1.length)
+while (lista1.length !== lista2.length){
+    minArray.push(Math.ceil(Math.random() * 10));
+}
 
-console.log(lista2)
-console.log(lista2.length)
+console.log(lista1);
+console.log(lista2);
