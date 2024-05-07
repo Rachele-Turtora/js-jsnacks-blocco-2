177c8b2da3cc573462c0441715cb3de53f1630cf
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-07 10:19:26 +0200

    [FIX]

--- snack-4/js/main.js
+++ snack-4/js/main.js
@@ -5,10 +5,16 @@ fino a quando ne avrà tanti quanti l’altro.*/
 "use strict";
 
 const lista1 = [3, 5, 1, 8, 2, 4, 5]
-const lista2 = [0, 5, 2, 8, 5, 6, 1, 3, 5, 9]
+const lista2 = [0, 5, 2, 8, 5, 4, 7, 6, 1, 2, 4]
 
-while (lista1.length < lista2.length){
-    lista1.push(Math.ceil(Math.random() * 10))
+if (lista1.length < lista2.length){
+    while (lista1.length < lista2.length){
+        lista1.push(Math.ceil(Math.random() * 10))
+    }
+} else {
+    while (lista2.length < lista1.length){
+        lista2.push(Math.ceil(Math.random() * 10))
+    }
 }
 
 console.log(lista1)
